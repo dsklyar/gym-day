2ce5faa9c0e1b872d760d0ca4dc71b66b2b95668
Daniel <danielksklyar@gmail.com>
2019-12-26 22:38:28 -0800

    Fixed Error for "@" module resolution in React Native Android -The issue is in babelrc is used for android bundle, not webpack

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,13 +1,13 @@
-import { ExerciseListComponent } from "@/components/exercise-list.component";
+import { WorkoutListComponent } from "@/components/workout-list.component";
 import { statusBarHeight, usableHeight, windowWidth } from "@/dimensions";
-import { IExerciseEntry } from "@/interfaces";
+import { IWorkoutEntry } from "@/interfaces";
 import { useTypedSelector } from "@/reducers";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export const LandingPage: React.FC = () => {
-	const data = useTypedSelector<IExerciseEntry[]>(
-		(state) => state.app.currentRoutine.workouts[0].exercises
+	const data = useTypedSelector<IWorkoutEntry[]>(
+		(state) => state.app.currentRoutine.workouts
 	);
 	return (
 		<View style={styles.container}>
@@ -15,7 +15,7 @@ export const LandingPage: React.FC = () => {
 				<Text>March 20th, Leg Day</Text>
 			</View>
 			<View style={styles.list}>
-				<ExerciseListComponent data={data} />
+				<WorkoutListComponent data={data} />
 			</View>
 			<View style={styles.toolbar}>
 				<Text>March 20th, Leg Day</Text>
--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,17 @@ module.exports = async function(env, argv) {
 	// If you want to add a new alias to the config.
 	// NOTE: This is needs to be set in order to get '@' working
 	config.resolve.alias["@"] = join(__dirname, "src/");
-	// console.log(config.resolve.alias);
+	console.log(config.resolve.alias);
 
 	// Maybe you want to turn off compression in dev mode.
 	if (config.mode === "development") {
+		console.log("DEV");
 		config.devServer.compress = false;
 	}
 
 	// Or prevent minimizing the bundle when you build.
 	if (config.mode === "production") {
+		console.log("PROD");
 		config.optimization.minimize = false;
 	}
 
