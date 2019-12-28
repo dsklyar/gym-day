39257502cba6355db71fdc72a1950b32aace75aa
Daniel <danielksklyar@gmail.com>
2019-12-28 02:27:24 -0800

    TypeSafe actions rework -The package has updated to 5.1 -Rewrote usign new syntax

--- src/actions/app.actions.ts
+++ src/actions/app.actions.ts
@@ -2,22 +2,10 @@ import { IExerciseEntry, IWorkoutEntry } from "@/interfaces";
 import { createAction } from "typesafe-actions";
 import { ActionTypes } from "./action-types";
 
-export const createExerciseAction = createAction(
-	ActionTypes.APP.CREATE_EXERCISE,
-	(resolve) => (payload: IExerciseEntry) => resolve(payload)
-);
+export const createExerciseAction = createAction(ActionTypes.APP.CREATE_EXERCISE)<IExerciseEntry>();
 
-export const removeExerciseAction = createAction(
-	ActionTypes.APP.REMOVE_EXERCISE,
-	(resolve) => (payload: IExerciseEntry) => resolve(payload)
-);
+export const removeExerciseAction = createAction(ActionTypes.APP.REMOVE_EXERCISE)<IExerciseEntry>();
 
-export const createWorkoutAction = createAction(
-	ActionTypes.APP.CREATE_WORKOUT,
-	(resolve) => (payload: IWorkoutEntry) => resolve(payload)
-);
+export const createWorkoutAction = createAction(ActionTypes.APP.CREATE_WORKOUT)<IWorkoutEntry>();
 
-export const removeWorkoutAction = createAction(
-	ActionTypes.APP.REMOVE_WORKOUT,
-	(resolve) => (payload: IWorkoutEntry) => resolve(payload)
-);
+export const removeWorkoutAction = createAction(ActionTypes.APP.REMOVE_WORKOUT)<IWorkoutEntry>();
