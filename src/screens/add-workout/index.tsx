13b360d33f30d33ee3445b939b5cf1f4adf2a074
Daniel <danielksklyar@gmail.com>
2019-12-29 10:37:42 -0800

    Added add wprkout screen with basic func

--- src/screens/add-workout/index.tsx
+++ src/screens/add-workout/index.tsx
@@ -1,16 +1,52 @@
+import actions from "@/actions";
 import { statusBarHeight, usableHeight, windowWidth } from "@/dimensions";
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components/native";
 
+
 export const AddWorkoutScreen: React.FC = () => {
+	const dispatch = useDispatch();
+	const [workoutName, onChangeText] = useState<string>("");
+	const createNewWorkout = useCallback((): void => {
+		dispatch(actions.App.createWorkoutAction({ name: workoutName, exercises: [] }));
+	}, [dispatch, workoutName]);
+
+	// TODO: Create a check if input is empty!
 	return (
-		<Container/>
+		<Container>
+			<GripContainer />
+			<DisplayContainer>
+				<WorkoutNameTextInput
+					value={workoutName}
+					onChangeText={(text: string) => onChangeText(text)} 
+					placeholder={"New Workout"} />
+			</DisplayContainer>
+			<Confirm title={"Confirm"} onPress={createNewWorkout} />
+		</Container>
 	);
 };
+
+const Confirm = styled.Button`
+`;
+
+const WorkoutNameTextInput = styled.TextInput`
+	height: 40px;
+	border-width: 1px;
+	border-color: grey;
+`;
+
+const GripContainer = styled.View`
+	flex: 1;
+	background-color: orange;
+`;
+
+const DisplayContainer = styled.View`
+	flex: 3;
+`;
 
 const Container = styled.View`
 	height: ${usableHeight};
 	width: ${windowWidth};
 	margin-top: ${statusBarHeight};
-	background-color: orange
 `;
